17c6fbdbb5178661788de05ea8e378b489efdd06
ClayCore <claymorealt@op.pl>
2019-10-18 00:11:49 +0200

    Updates

---
+++ precache-manifest.e81e5dfbcff0b9ca3fa67f43828b0531.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7444d2fb00f2a57f5a806cb8c35374ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a537dd7d4d465f67f77",
+    "url": "/static/css/main.4c3b08cf.chunk.css"
+  },
+  {
+    "revision": "668f174b10bb57180711",
+    "url": "/static/js/2.02a0e263.chunk.js"
+  },
+  {
+    "revision": "8a537dd7d4d465f67f77",
+    "url": "/static/js/main.d9c97024.chunk.js"
+  },
+  {
+    "revision": "8c250d1a2183e915b423",
+    "url": "/static/js/runtime-main.d4bd6127.js"
+  }
+]);
